1049d64c33a5a85e38a0de32f484124eb1555d60
analuizaalves <alves.luiza09@aluno.ifce.edu.br>
2025-01-13 16:59:50 -0300

    meus arquivos django

--- basico/settings.py
+++ basico/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!xjs2#=m1vbd53-xz78b8q(35*_v@-avj)272pf(jed#s4)59'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
